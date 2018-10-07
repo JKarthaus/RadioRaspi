30d55ab2ab96acee061037b2c19171cd42013c12
jkarthaus <joern.karthaus@gmail.com>
2018-10-07 18:52:10 +0200

    complete localInstall script

--- localInstall.sh
+++ localInstall.sh
@@ -2,4 +2,15 @@
 
 # Pre Install these Python tools
 
+echo "install python tools"
 sudo apt-get install python-smbus i2c-tools
+
+echo "set shutdown Display links"
+sudo ln -s /home/volumio/lcd/lcdShutdown.service /etc/systemd/system/halt.target.wants/lcdShutdown.service
+sudo ln -s /home/volumio/lcd/lcdShutdown.service /etc/systemd/system/poweroff.target.wants/lcdShutdown.service
+
+echo "install mpcLCD service"
+sudo copy mpcLcdService /etc/init.de
+sudo update-rc.d mpcLcdService defaults
+
+
