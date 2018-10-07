1c27ebbc041581101e69a0155fb76b7f9cf9ff88
jkarthaus <joern.karthaus@gmail.com>
2018-10-07 19:23:21 +0200

    add blinkt stuff

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "copy lcd directory python Scripts to Radio Raspi"
-
 scp -r lcd volumio@radioRaspi:~
+
+echo "copy blinkt directory python Scripts to Radio Raspi"
+scp -r blinkt volumio@radioRaspi:~
 
 
 echo "copy Service Script to Radio Raspi"
-
 scp mpcLcdService volumio@radioRaspi:~
-
 scp localInstall.sh volumio@radioRaspi:~
--- localInstall.sh
+++ localInstall.sh
@@ -10,6 +10,9 @@ fi
 echo "install python tools"
 apt-get install python-smbus i2c-tools
 
+echo "numpy is required for illumination"
+pip install numpy
+
 echo "set shutdown Display links"
 ln -s /home/volumio/lcd/lcdShutdown.service /etc/systemd/system/halt.target.wants/lcdShutdown.service
 ln -s /home/volumio/lcd/lcdShutdown.service /etc/systemd/system/poweroff.target.wants/lcdShutdown.service
