f27522687380ebe6023a7d8a53ee60799c54e583
jkarthaus <joern.karthaus@gmail.com>
2018-10-07 19:06:42 +0200

    fix typo

--- localInstall.sh
+++ localInstall.sh
@@ -2,15 +2,21 @@
 
 # Pre Install these Python tools
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run the Script via sudo or as root"
+  exit
+fi
+
 echo "install python tools"
-sudo apt-get install python-smbus i2c-tools
+apt-get install python-smbus i2c-tools
 
 echo "set shutdown Display links"
-sudo ln -s /home/volumio/lcd/lcdShutdown.service /etc/systemd/system/halt.target.wants/lcdShutdown.service
-sudo ln -s /home/volumio/lcd/lcdShutdown.service /etc/systemd/system/poweroff.target.wants/lcdShutdown.service
+ln -s /home/volumio/lcd/lcdShutdown.service /etc/systemd/system/halt.target.wants/lcdShutdown.service
+ln -s /home/volumio/lcd/lcdShutdown.service /etc/systemd/system/poweroff.target.wants/lcdShutdown.service
 
 echo "install mpcLCD service"
-sudo copy mpcLcdService /etc/init.de
-sudo update-rc.d mpcLcdService defaults
+cp mpcLcdService /etc/init.de
+echo "Set mpcLcdService as default start Service"
+update-rc.d mpcLcdService defaults
 
 
