ccc324ba01aaad5690aa088d2873c5c799877b22
jkarthaus <joern.karthaus@gmail.com>
2018-11-23 21:30:29 +0100

    add playlist parsing function

--- hue/hueConnector.py
+++ hue/hueConnector.py
@@ -8,6 +8,7 @@ import logging
 import subprocess
 import json
 import glob
+import os
 
 # create logger
 logger = logging.getLogger('hueConnector')
@@ -18,18 +19,32 @@ def parseActualSong ():
     parsed_json = json.loads(p.communicate()[0])
     
     if parsed_json['status'] == "play" :
-        return parsed_json['uri']
+        return parsed_json['title']
     else :
         return "NONE"
 
-def parseHuePlaylists(songURI):
+def parseHuePlaylistsForScene(songURI):
     logger.info("Parsing playlists beginning with hue* and Song " + songURI)
-    print(glob.glob("/home/joern/git/RadioRaspi/hue/hue_*"))
-    
-    
+    for filename in glob.glob("playlist/hue_*"):
+        with open(filename) as playlist_file:
+            logger.info("Checking Filename:" + filename)
+            parsed_json = json.load(playlist_file)
+            for items in parsed_json:
+                if items["title"] == songURI :
+                    scene = os.path.basename(filename)
+                    scene = os.path.splitext(scene)[0]
+                    scene = scene[4:]
+                    return scene
+    return "NONE"            
+
+
+def selectHueScene(scene):
+    logger.info("Switch to Scene : ")    
+
+            
 if __name__ == '__main__':
     actualSong = parseActualSong()
     if actualSong != "NONE":
-        parseHuePlaylists(actualSong)
+        print parseHuePlaylistsForScene(actualSong)
     
     pass
